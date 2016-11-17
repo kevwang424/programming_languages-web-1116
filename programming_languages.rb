9158bbde130ea367b8d2f54f28404ded488a1fac
Kevin Wang <kev.wang424@gmail.com>
2016-11-17 17:20:32 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,52 @@
-def reformat_languages(languages)
-  # your code here
+require 'byebug'
+def languages
+  {:oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
 end
+
+def reformat_languages(languages)
+
+  new_hash = Hash.new()
+
+  languages.each do |style, lang|
+    #debugger
+    lang.each do |language, type|
+      unless new_hash.include?(language)
+        new_hash[language] = type
+        new_hash[language][:style] = []       #declaring :style value as an array
+      end
+      #debugger if style == :functional && language == :javascript
+      new_hash[language][:style] << style
+    end
+  end
+  new_hash
+end
+#p reformat_languages(languages)
